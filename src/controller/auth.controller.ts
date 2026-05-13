f150525a1aed11dd945bdfeec3fe5d2a3b5b451f
Shaedow2000 <shaedow2000@proton.me>
2026-05-13 22:17:40 +0100

    Create login controller.

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,6 +2,10 @@ import type { Response, Request } from "express";
 import wrapper from "../middlewares/asyncWrapper.middleware.ts";
 import crypto from "crypto";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+import * as z from "zod";
+import "dotenv/config";
 
 import AccountModel from "../model/account.ts";
 import Mailer from "../config/mail.ts";
@@ -116,16 +120,72 @@ const login = wrapper(
     const result = AccountZodObject.safeParse({ email, password });
 
     if (!result.success) {
-      logger.error(result.error);
+      logger.error(z.prettifyError(result.error));
 
       return res.status(400).json({
         status: 400,
-        message: result.error,
+        message: z.flattenError(result.error).fieldErrors,
       });
     }
 
-    return res;
+    const account = await AccountModel.findOne({ email }, { __v: false });
+
+    if (!account) {
+      logger.error("Account not found");
+      return res.status(404).json({
+        status: 404,
+        message: "Account not found",
+      });
+    }
+
+    if (!account.isVerified) {
+      logger.warn({ message: "Account is not verified", account: email });
+      return res.status(401).json({
+        status: 401,
+        message: "Account is not verified",
+      });
+    }
+
+    const correctPassword: boolean = await bcrypt.compare(
+      password ?? "",
+      account.password,
+    );
+
+    if (!correctPassword) {
+      logger.error({
+        message: "Email or password are incorrect",
+        account: email,
+      });
+      return res.status(401).json({
+        status: 401,
+        message: "Email or password are incorrect",
+      });
+    }
+
+    const accessToken = jwt.sign(
+      { email: email },
+      process.env.JWT_SECRET ?? "",
+      { expiresIn: "30m" },
+    );
+
+    const refreshToken: { token: string; expiry: Date } = {
+      token: uuidv4(),
+      expiry: new Date(Date.now() + 30 * 24 * 60 * 1000),
+    };
+
+    res.cookie("Refresh-Token-Id", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+    });
+
+    return res.status(200).json({
+      status: 200,
+      message: "Logged in successfully",
+      account: account,
+      token: accessToken,
+    });
   },
 );
 
-export { register, verifyAccount };
+export { register, verifyAccount, login };
